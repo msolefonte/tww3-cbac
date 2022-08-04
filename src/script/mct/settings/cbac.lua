abd4e9d0671df1cdf7bd4820cadc5878658a9b8c
Marc Solé <msolefonte@gmail.com>
2022-08-04 18:16:34 +0200

    Update 1.3.0 (#4)
    - Added information about unit costs in the Info button
    - Added support for localization. Now the mod can be translated!
    - Improved optimization, reducing AI turns time
    - Fixed a bug that caused Tooltip texts to also appear on heroes without armies
    - Updated CHANGELOG.md format
    - Technical changes:
      - New method `gls(localised_string_key)` added to the CBAC lib API

--- src/script/mct/settings/cbac.lua
+++ src/script/mct/settings/cbac.lua
@@ -10,12 +10,6 @@ mct_mod:set_description("MP-Style Cost Limit For All Armies", false);
 
 local mct_section_base = mct_mod:add_new_section("1-cbac-base", "Base Options", false);
 
-local option_cbac_settings_locked = mct_mod:add_new_option("settings_locked", "checkbox");
-option_cbac_settings_locked:set_text("Lock settings during campaigns");
-option_cbac_settings_locked:set_tooltip_text("If enabled, you can't change these settings during a campaign, only from the main menu.");
-option_cbac_settings_locked:set_default_value(false);
-option_cbac_settings_locked:set_read_only(true);
-
 local option_cbac_army_limit_player = mct_mod:add_new_option("army_limit_player", "slider");
 option_cbac_army_limit_player:set_text("Player Limit (Base)");
 option_cbac_army_limit_player:set_tooltip_text("How many points per player army?");
@@ -36,6 +30,12 @@ option_cbac_hero_cap:set_tooltip_text("Exceeding this number of embedded heroes 
 option_cbac_hero_cap:slider_set_min_max(0, 19);
 option_cbac_hero_cap:slider_set_step_size(1);
 option_cbac_hero_cap:set_default_value(2);
+
+local option_cbac_settings_locked = mct_mod:add_new_option("settings_locked", "checkbox");
+option_cbac_settings_locked:set_text("Lock settings during campaigns");
+option_cbac_settings_locked:set_tooltip_text("If enabled, you can't change these settings during a campaign, only from the main menu.");
+option_cbac_settings_locked:set_default_value(false);
+option_cbac_settings_locked:set_read_only(true);
 
 local mct_section_dl = mct_mod:add_new_section("2-cbac-dl", "Dynamic Limits Options", false);
 
@@ -86,7 +86,7 @@ option_cbac_autolevel_ai:slider_set_min_max(0, 5);
 option_cbac_autolevel_ai:slider_set_step_size(1);
 option_cbac_autolevel_ai:set_default_value(3);
 
-local mct_section_sl = mct_mod:add_new_section("4-cbac-sl", "Cost-based Supply Lines Options [BETA]", false);
+local mct_section_sl = mct_mod:add_new_section("4-cbac-sl", "Cost-based Supply Lines Options", false);
 
 local option_cbac_supply_lines = mct_mod:add_new_option("supply_lines", "checkbox");
 option_cbac_supply_lines:set_text("Enable Cost-based Supply Lines");
